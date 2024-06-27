0d28b5e3ec32339144629a197f1df511ee84dc1e
shahbazhashmi14a <shahbazhashmi14a@gmail.com>
2024-06-27 05:10:01 +0000

    update

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ print("Welcome to Quiz Bot. Get ready to challenge your knowledge with our excit
 
 
 # Program Start here:
-GOOGLE_API_KEY='AIzaSyC_1F8N1oLYOXvv_MJ21Yp0GlRU6ksT2R4'
+GOOGLE_API_KEY='API_KEY'
 
 genai.configure(api_key=GOOGLE_API_KEY) #apikey configuration
 
