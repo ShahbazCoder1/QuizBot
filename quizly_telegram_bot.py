700caaa821d0e808e04ec702559f59264cea4e2a
shahbazhashmi14a <shahbazhashmi14a@gmail.com>
2024-07-14 08:28:45 +0000

    Fixed minor bugs, Added Feedback command, and context.user_data added for efficiency

--- quizly_telegram_bot.py
+++ quizly_telegram_bot.py
@@ -1,18 +1,11 @@
 import google.generativeai as genai
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import time
 from typing import Final
 from telegram import Update, Poll, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, PollHandler, CallbackQueryHandler, filters
-
-SUBJECT = None
-TOPIC = None 
-LEVEL = None
-STATE = None
-c_id = None
-quiz = None
-question_index = 0
-cor = 0
-incor = 0
 
 
 TOKEN: Final = '7068344943:AAEIFEtmH0N64n7ombEPfDcMpPCOYwN3WFU'
@@ -20,7 +13,63 @@ BOT_USERNAME: Final = '@Quisly_Bot'
 
 print("  ____          _ \n / __ \\        (_)\n| |  | | _   _  _  ____\n| |  | || | | || ||_  /\n| |__| || |_| || | / /_\n \\___\\_\\ \\__,_||_|/____|\n ____          _   \n|  _ \\        | |  \n| |_) |  ___  | |_ \n|  _ <  / _ \\ | __|\n| |_) || (_) || |_ \n|____/  \\___/  \\__|")
 
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Here are the available commands: \n/start - Begin with your quiz \n/help - Show this help message \n/about - See a description of the bot \n/stop- Stop the quiz")
+
+async def about(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("You can use this bot to take quizzes on different subjects across various topics. You can also adjust the difficulty level according to your convenience. \n\nYour score will be displayed after you finish the quiz. \n\nDEVELOPER INFO: \n-MD SHAHBAZ HASHMI ANSARI (https://github.com/ShahbazCoder1)\n-VIDHI AGARWAL (https://github.com/Vidhi-28) \n\nSource Code at: https://github.com/ShahbazCoder1/QuizBot")
+
+async def feedback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Enter your Feedback message: ")
+    
+    # Store the chat_id to use it later
+    context.user_data['waiting_for_feedback'] = True
+    
+async def handle_feedback_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if context.user_data.get('waiting_for_feedback'):
+        from_email = 'quizlyfeedback@gmail.com'
+        from_password = 'htlq kwaj wekx pvsl'
+        msg = MIMEMultipart()
+        msg['From'] = from_email
+        msg['To'] = "shahbazhashmi14a@gmail.com , shahbazhashmiansari@gmail.com"
+        msg['Subject'] = "Someone has given a feedback about Quizly Bot"
+        body = update.message.text
+        # Attach the message body
+        msg.attach(MIMEText(body, 'plain'))
+
+        # Set up the SMTP server
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()  # Enable security
+        server.login(from_email, from_password)
+
+        # Send the email
+        server.send_message(msg)
+        server.quit()
+
+        await update.message.reply_text("Thank you for your feedback!")
+        context.user_data['waiting_for_feedback'] = False
+    else:
+        await handle_message(update, context)
+
+async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    question_index = context.user_data.get('question_index')
+    c_id = context.user_data.get('c_id')
+    question_index=len(quiz)+1
+    await loadQuiz(c_id,update, context)
+
+async def resetAll(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    context.user_data['SUBJECT'] = None
+    context.user_data['TOPIC'] = None 
+    context.user_data['LEVEL'] = None
+    context.user_data['STATE'] = None
+    context.user_data['c_id'] = await get_chat_id(update, context)
+    context.user_data['quiz'] = None
+    context.user_data['question_index'] = 0
+    context.user_data['cor'] = 0
+    context.user_data['incor'] = 0
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await resetAll(update, context)
     await update.message.reply_text("Get ready for Quizly Quiz!")
     keyboard = [
         ["Mathematics", "Physics"],
@@ -34,22 +83,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         resize_keyboard=True,
         one_time_keyboard=False
     )
-
     await update.message.reply_text("Enter the subject you wish to take the quiz on: ", reply_markup=reply_markup)
-    global STATE
-    STATE = "subject"
+    context.user_data['STATE'] = "subject"
 
 async def subj(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    global SUBJECT
-    global STATE
-    SUBJECT = update.message.text
+    context.user_data['SUBJECT'] = update.message.text
     await update.message.reply_text("Enter the topic you wish to take a quiz on: ")
-    STATE = "topic"
+    context.user_data['STATE'] = "topic"
 
 async def topi(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    global TOPIC
-    global STATE
-    TOPIC = update.message.text
+    context.user_data['TOPIC'] = update.message.text
     keyboard = [
         [InlineKeyboardButton("Beginner", callback_data="Beginner")],
         [InlineKeyboardButton("Intermediate", callback_data="Intermediate")],
@@ -57,36 +100,13 @@ async def topi(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text("Enter the level of the quiz: ", reply_markup=reply_markup)
-    STATE = "level"
-
-async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Here are the available commands: \n/start - Begin with your quiz \n/help - Show this help message \n/about - See a description of the bot \n/stop- Stop the quiz")
-
-
-async def about(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("You can use this bot to take quizzes on different subjects across various topics. You can also adjust the difficulty level according to your convenience. \n\nYour score will be displayed after you finish the quiz. \n\nDEVELOPER INFO: \n-MD SHAHBAZ HASHMI ANSARI (https://github.com/ShahbazCoder1)\n-VIDHI AGARWAL (https://github.com/Vidhi-28) \n\nSource Code at: https://github.com/ShahbazCoder1/QuizBot")
-
-async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    global cor,incor,question_index,c_id
-    question_index=len(quiz)+1
-    await loadQuiz(c_id,update, context)
-    await resetAll()
-
-async def resetAll() -> None:
-    global SUBJECT, TOPIC, LEVEL, STATE, c_id, quiz, question_index, cor, incor
-    SUBJECT = None
-    TOPIC = None 
-    LEVEL = None
-    STATE = None
-    c_id = None
-    quiz = None
-    question_index = 0
-    cor = 0
-    incor = 0
+    context.user_data['STATE'] = "level"
 
 async def leve(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    global SUBJECT, TOPIC, LEVEL, STATE, c_id, quiz
-    c_id = await get_chat_id(update, context)
+    SUBJECT = context.user_data.get('SUBJECT')
+    TOPIC = context.user_data.get('TOPIC')
+    LEVEL = context.user_data.get('LEVEL')
+    c_id = context.user_data.get('c_id')
     message = await context.bot.send_message(chat_id=c_id, text="Generating quiz questions, please wait...")
     # Program Start here:
     GOOGLE_API_KEY='AIzaSyC_1F8N1oLYOXvv_MJ21Yp0GlRU6ksT2R4'
@@ -118,14 +138,12 @@ async def leve(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     }}
 
     Ensure that all questions are related to the specified topic and subject, and match the specified difficulty level. Return only the Python dictionary code, without any additional text or explanations.""")
-
-    quiz = eval(response.text.replace("```","").replace("python","").replace(f"{TOPIC.lower()}_questions = ",''))
+    context.user_data['quiz'] = eval(response.text.replace("```","").replace("python","").replace(f"{TOPIC.lower()}_questions = ",''))
     await message.edit_text("Let's Begin")
-    C = 0
     await loadQuiz(c_id,update, context)
 
 #obtain chat id 
-async def get_chat_id(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def get_chat_id(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     chat_id = -1
     if update.message is not None:
         chat_id = update.message.chat.id
@@ -135,26 +153,43 @@ async def get_chat_id(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
         chat_id = context.bot_data[update.poll.id]
     return chat_id
 
-async def poll_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:  
-    global c_id, cor, incor
+async def poll_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        # Check if context.user_data exists
+    if context.user_data is None:
+        print("Warning: context.user_data is None")
+        return  # Exit the function if there's no user data
+
+    c_id = context.user_data.get('c_id')
+    
+    # If c_id is not found in user_data, try to get it from the update
+    if c_id is None:
+        c_id = await get_chat_id(update, context)
+        if c_id == -1:
+            print("Error: Unable to determine chat_id")
+            return 
+            
     correct_answer = update.poll.correct_option_id
     option_1_text = update.poll.options[correct_answer].text
     answers = update.poll.options
+    await ansCheck(answers,option_1_text, context)
     ret = ""
     for answer in answers:
         if answer.voter_count ==1:
             ret = answer.text
             break
     if option_1_text == ret:
-        cor +=1
+        context.user_data['cor'] += 1 
         print("correct")
     else:
-        incor +=1
+        context.user_data['incor'] += 1 
         print("incorrect")
     await loadQuiz(c_id,update, context)
 
 async def loadQuiz(c_id, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    global quiz, question_index, cor, incor
+    quiz = context.user_data.get('quiz') 
+    cor = context.user_data.get('cor')
+    incor = context.user_data.get('incor')
+    question_index = context.user_data.get('question_index')
     question_index += 1
     if question_index <= len(quiz):
         question_key = f"Q{question_index}"
@@ -166,20 +201,20 @@ async def loadQuiz(c_id, update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
         message = await context.bot.send_poll(chat_id=c_id, question=f"\n{question_index}: {value['question']}",options=opt, type=Poll.QUIZ, correct_option_id=answer_index)
     else:
         await context.bot.send_message(chat_id=c_id, text=f"Quiz Completed.\nNumber of correct answers: {cor} \nNumber of incorrect answers:{incor}")
-        await resetAll()
+        await resetAll(update, context)
 
 async def handle_input(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    global STATE, SUBJECT, LEVEL
+    STATE = context.user_data.get('STATE')
     query = update.callback_query
     await query.answer()
     if STATE == "level":
-        LEVEL = query.data
+        context.user_data['LEVEL'] = query.data
         await leve(update, context)
     else:
         await query.message.reply_text("Invalid input. Please try again.")
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    global STATE
+    STATE = context.user_data.get('STATE')
     if STATE == "subject":
         await subj(update, context)
     elif STATE == "topic":
@@ -193,6 +228,8 @@ def main():
     application.add_handler(CommandHandler("help", help))
     application.add_handler(CommandHandler("about", about))
     application.add_handler(CommandHandler("stop", stop))
+    application.add_handler(CommandHandler("feedback", feedback))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_feedback_message))
     application.add_handler(CallbackQueryHandler(handle_input))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     application.add_handler(PollHandler(poll_handler))
